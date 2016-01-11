9f32b53903eef78f3cd35c831c1d6db2d20c3191
danthonius <danthonius@yahoo.com>
2016-01-11 08:56:56 +0200

    Am modificat cateva comentarii.

--- lab12_POO/Daoud-Moraru_Anthonius.cpp
+++ lab12_POO/Daoud-Moraru_Anthonius.cpp
@@ -5,19 +5,20 @@ class A {};
 
 class B {
 public:
-  // conversion from A (constructor):
+
   B (const A& x) {}
-  // conversion from A (assignment):
+
   B& operator= (const A& x) {return *this;}
-  // conversion to A (type-cast operator)
+
   operator A() {return A();}
 };
 
 int main ()
 {
+
   A foo;
-  B bar = foo;    // calls constructor
-  bar = foo;      // calls assignment
-  foo = bar;      // calls type-cast operator
+  B bar = foo;
+  bar = foo;
+  foo = bar;
   return 0;
 }
